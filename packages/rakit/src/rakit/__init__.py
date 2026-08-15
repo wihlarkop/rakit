732784fd126ac0bf1fcf1657068c2d9f1ddfd215
Wihlarko Prasdegdho <wihlarko26@gmail.com>
2026-08-16 00:00:46 +0700

    feat: expose resource composition contracts

--- packages/rakit/src/rakit/__init__.py
+++ packages/rakit/src/rakit/__init__.py
@@ -1,18 +1,40 @@
-from rakit_core.actions import ActionResult, ActionSuccess
+from rakit_core.actions import (
+    ActionDefinition,
+    ActionResult,
+    ActionScope,
+    ActionSuccess,
+)
 from rakit_core.admin_types import ModelAdmin, ResourceAdmin
+from rakit_core.bulk import BulkExecutionPolicy, BulkPolicy
 from rakit_core.config import RakitConfig, SecretValue
 from rakit_core.errors import RakitError
+from rakit_core.relationships import (
+    RelationshipCardinality,
+    RelationshipDefinition,
+    RelationshipDestructivePolicy,
+    RelationshipEditMode,
+    RelationshipKind,
+)
 from rakit_web.admin import Admin
 
 __version__ = "0.1.0a1"
 
 __all__ = [
+    "ActionDefinition",
     "ActionResult",
+    "ActionScope",
     "ActionSuccess",
     "Admin",
+    "BulkExecutionPolicy",
+    "BulkPolicy",
     "ModelAdmin",
     "RakitConfig",
     "RakitError",
+    "RelationshipCardinality",
+    "RelationshipDefinition",
+    "RelationshipDestructivePolicy",
+    "RelationshipEditMode",
+    "RelationshipKind",
     "ResourceAdmin",
     "SecretValue",
     "__version__",
