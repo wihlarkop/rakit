4b2005f0f66ebbf43a6ad1d7f9cb6d8dc1b63595
Wihlarko Prasdegdho <wihlarko26@gmail.com>
2026-08-20 03:10:46 +0700

    feat(facade): expose neutral storage contracts

---
+++ packages/rakit/src/rakit/storage/__init__.py
@@ -0,0 +1,9 @@
+from rakit_storage import DeleteBehavior, FileAccess, FileStorage, StoredFile, TemporaryUpload
+
+__all__ = [
+    "DeleteBehavior",
+    "FileAccess",
+    "FileStorage",
+    "StoredFile",
+    "TemporaryUpload",
+]
