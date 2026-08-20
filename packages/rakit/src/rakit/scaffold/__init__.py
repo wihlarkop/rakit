bb8f97f307354fb34d6b8b879478064306396c57
Wihlarko Prasdegdho <wihlarko26@gmail.com>
2026-08-21 05:07:43 +0700

    feat(cli): expose C2 scaffold rendering helpers

--- packages/rakit/src/rakit/scaffold/__init__.py
+++ packages/rakit/src/rakit/scaffold/__init__.py
@@ -18,6 +18,12 @@ from .model import (
     ServerAdapter,
     StarterTemplate,
 )
+from .render import (
+    dependency_action_for,
+    dependency_command_for,
+    guidance_for,
+    render_scaffold_files,
+)
 
 __all__ = [
     "ApplyResult",
@@ -32,8 +38,12 @@ __all__ = [
     "ScaffoldPlan",
     "ServerAdapter",
     "StarterTemplate",
+    "dependency_action_for",
+    "dependency_command_for",
     "detect_host_framework",
+    "guidance_for",
     "normalize_distribution_name",
+    "render_scaffold_files",
     "resolve_existing_package",
     "validate_package_name",
 ]
