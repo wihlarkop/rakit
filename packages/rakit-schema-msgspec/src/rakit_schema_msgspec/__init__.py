9b12198d312c5e3d0ffb0a865d49468f81cd495d
Wihlarko Prasdegdho <wihlarko26@gmail.com>
2026-08-22 04:25:41 +0700

    feat(schema): expose msgspec schema adapter

---
+++ packages/rakit-schema-msgspec/src/rakit_schema_msgspec/__init__.py
@@ -0,0 +1,9 @@
+from .adapter import MsgspecSchemaAdapter
+from .capabilities import MSGSPEC_SCHEMA_CAPABILITIES
+from .discovery import MSGSPEC_INTEGRATION
+
+__all__ = [
+    "MSGSPEC_INTEGRATION",
+    "MSGSPEC_SCHEMA_CAPABILITIES",
+    "MsgspecSchemaAdapter",
+]
