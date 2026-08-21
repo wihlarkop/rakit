b99e000b81ebfef018744210141f3c86ffa11631
Wihlarko Prasdegdho <wihlarko26@gmail.com>
2026-08-22 04:24:26 +0700

    refactor(schema): expose pydantic schema adapter

---
+++ packages/rakit-schema-pydantic/src/rakit_schema_pydantic/__init__.py
@@ -0,0 +1,9 @@
+from .adapter import PydanticSchemaAdapter
+from .capabilities import PYDANTIC_SCHEMA_CAPABILITIES
+from .discovery import PYDANTIC_INTEGRATION
+
+__all__ = [
+    "PYDANTIC_INTEGRATION",
+    "PYDANTIC_SCHEMA_CAPABILITIES",
+    "PydanticSchemaAdapter",
+]
