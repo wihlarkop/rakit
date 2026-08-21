bf5bf435960f7c55ee56037fece1c844b1867f59
Wihlarko Prasdegdho <wihlarko26@gmail.com>
2026-08-22 05:44:27 +0700

    feat(tortoise): expose adapter package

---
+++ packages/rakit-tortoise/src/rakit_tortoise/__init__.py
@@ -0,0 +1,11 @@
+from .capabilities import TORTOISE_CAPABILITIES
+from .datasource import TortoiseDataSource
+from .discovery import TORTOISE_INTEGRATION
+from .plugin import TortoisePlugin
+
+__all__ = [
+    "TORTOISE_CAPABILITIES",
+    "TORTOISE_INTEGRATION",
+    "TortoiseDataSource",
+    "TortoisePlugin",
+]
