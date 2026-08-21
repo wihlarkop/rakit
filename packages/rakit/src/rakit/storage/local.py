7d877781fb1ca007e5321883b81bd5efe44be4b1
Wihlarko Prasdegdho <wihlarko26@gmail.com>
2026-08-21 10:08:27 +0700

    feat: use canonical storage install hint

--- packages/rakit/src/rakit/storage/local.py
+++ packages/rakit/src/rakit/storage/local.py
@@ -1,6 +1,12 @@
-from .._optional import optional_import
+from .._install import InstallExtra
+from .._optional import OptionalDependency, optional_import
 
-with optional_import("rakit_storage_local", extra="storage-local"):
+_DEPENDENCY = OptionalDependency(
+    extra=InstallExtra.STORAGE_LOCAL,
+    label="Local storage",
+)
+
+with optional_import("rakit_storage_local", dependency=_DEPENDENCY):
     import rakit_storage_local  # noqa: F401
 
 from rakit_storage_local import LocalStorage, LocalStoragePlugin
