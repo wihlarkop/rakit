356d348ea6af9e930c2d3945a4965bbbc732d1fc
Wihlarko Prasdegdho <wihlarko26@gmail.com>
2026-08-23 14:30:42 +0700

    test(piccolo): include first-party discovery entrypoint

--- packages/rakit/tests/test_integration_discovery.py
+++ packages/rakit/tests/test_integration_discovery.py
@@ -15,6 +15,7 @@ def test_first_party_installed_integrations_are_discovered_without_activation() 
     assert tuple(item.integration_id for item in discovered) == (
         "auth.sqlalchemy",
         "persistence.peewee",
+        "persistence.piccolo",
         "persistence.sqlalchemy",
         "persistence.sqlalchemy-core",
         "persistence.tortoise",
